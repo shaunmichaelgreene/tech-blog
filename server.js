d036f187d4319279c2e3daf5f7a9b5c88ffbf8d2
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-07-14 15:42:20 -0400

    models added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'secret123',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -21,11 +21,11 @@ const sess = {
 
 app.use(session(sess));
 
-// const helpers = require('./utils/helpers');
-// const hbs = exphbs.create({ helpers });
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
